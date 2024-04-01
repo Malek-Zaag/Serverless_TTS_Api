6bbcd74e5c7380387a2d1782e2eee494c3969546
Malek <zaag.malek1@gmail.com>
2024-04-01 22:41:47 +0200

    lambda working

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,29 @@
 import json
+import boto3
+import base64
 
+def generateAudioFromText(text):
+    polly= boto3.client('polly')
+    response= polly.synthesize_speech(  Text=text,
+                                        TextType = "text",
+                                        OutputFormat='mp3', 
+                                        VoiceId='Bianca')
+    audio_stream = response['AudioStream'].read()
+    audio_base64 = base64.b64encode(audio_stream).decode('utf-8')
+    bucket_name = "serverless-api-bucket24"
+    filename = "audios/audio.mp3"
+    bucket = boto3.client('s3').put_object(Bucket=bucket_name, Key=filename, Body=audio_stream)
+    return audio_base64
+    
 def lambda_handler(event, context):
     # TODO implement
+    audio_base64 = generateAudioFromText("Hello world!")
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        #'body': json.dumps(base64.b64encode(response).decode('utf-8')+)
+        'headers': {
+                'Content-Type': 'audio/mpeg',
+            },
+        'body': audio_base64,
+        'isBase64Encoded': True
     }
